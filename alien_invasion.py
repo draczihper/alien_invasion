ae001af89e0dd8eda4abefa13faa7d900a603b5e
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-01 21:05:51 +0300

    Add a run game method

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,3 +10,21 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(1200, 800)
         pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while(True):
+            # Watch for keyboard and mouse invents.
+            for event in pygame.event.get(): 
+                    if event.type == pygame.QUIT():
+                         sys.exit()
+
+            # Make the most recent drawn screen visible
+            pygame.display.flip()
+
+if __name__ == "__main__":
+     # Make a game instance, and run the game
+     ai = AlienInvasion()
+     ai.run_game()
+
+     
