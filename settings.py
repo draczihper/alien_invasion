43c969f257794929f7b03ddd1926177783248a8a
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-01 22:04:07 +0300

    Add a new settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+class Settings(self):
+    """A class to store all settings of Alien Invasion."""
+
+    def __init__(self):
+        """Initilize the game's settings."""
+        # Screen settings
+        self.screen_width = 900
+        self.screen_height = 500
+        self.bg_color = (230, 230, 230)
