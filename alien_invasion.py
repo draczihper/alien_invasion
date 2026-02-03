b54c099e5bfccfaedadd23facfb5be3460fbf9a7
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-03 19:33:24 +0300

    Edit check events, update screen and run game func

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,24 +16,38 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-
-    def run_game(self):
-        """Start the main loop for the game"""
-
-        while(True):
-            self._check_events()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recent drawn screen visible
-            pygame.display.flip()
+            
     def _check_events(self):
         """Respond  keypresses and mouse events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:    
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recent drawn screen visible
+        pygame.display.flip()
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while(True):
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
 if __name__ == "__main__":
      # Make a game instance, and run the game
