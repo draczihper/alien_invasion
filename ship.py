910ec63a2c29438ef8da806e879c54134487ba3b
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-03 20:34:00 +0300

    Edit the ship position by it's position

--- ship.py
+++ ship.py
@@ -4,8 +4,9 @@ class Ship:
     """A class to manage the ship"""
 
     def __init__(self, ai_game):
-        """Initilize the ship and set it's starting position"""
+        """Initialize the ship and set it's starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship and get it's rect
@@ -15,16 +16,23 @@ class Ship:
         #Start each new ship at the bottom center of the screen 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
             # Movement flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         """Update the ship position according to the movement flag"""
+        # Update the ship's x value, not the rect
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at it's current location"""
