7471511b0afbc01a5a4a6be2a9fcb57963d2f251
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-05 23:28:28 +0300

    Add settings to our game assets

--- settings.py
+++ settings.py
@@ -8,3 +8,12 @@ class Settings:
         self.screen_width = 900
         self.screen_height = 500
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1
+
+        # Bullet settins 
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
